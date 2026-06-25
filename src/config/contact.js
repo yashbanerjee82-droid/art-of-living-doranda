da54df27f727c4d709c428a71dce2d082da86f90
Yash Banerjee <yashbanerjee82@gmail.com>
2026-06-25 19:00:28 +0530

    Update centre address

--- src/config/contact.js
+++ src/config/contact.js
@@ -2,21 +2,21 @@ export const CONTACT_DETAILS = {
   name: "Art of Living Centre, Doranda",
   shortName: "Art of Living Doranda",
   address: {
-    line1: "123 Peace Avenue, Near Secretariat",
+    line1: "Sri Sri Ravishankar Picture, 92, North Office Para, Shyamali Colony",
     line2: "Doranda, Ranchi",
     state: "Jharkhand 834002, India",
     city: "Ranchi",
     region: "Jharkhand, India"
   },
   phones: [
-    "+91 98765 43210",
-    "+91 87654 32109"
+    "+91 89200 04690",
+    "+91 77639 95942"
   ],
   email: "info@artoflivingdoranda.org",
-  mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14656.32688049174!2d85.31464345!3d23.3128318!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f4e17621c0b395%3A0xcb1fc24c6e94916a!2sDoranda%2C%20Ranchi%2C%20Jharkhand!5e0!3m2!1sen!2sin!4v1700000000000!5m2!1sen!2sin",
+  mapUrl: "https://maps.app.goo.gl/BcrCWeESu5CFy9sEA",
   socials: {
     facebook: "https://facebook.com/artofliving",
-    instagram: "https://instagram.com/artofliving",
+    instagram: "https://www.instagram.com/artoflivingranchi?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
     twitter: "https://twitter.com/artofliving",
     youtube: "https://youtube.com/artofliving"
   }

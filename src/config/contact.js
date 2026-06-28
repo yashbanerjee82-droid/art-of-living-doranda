694cfc9dacc24fe7bfcb230ad91ade26525696c3
Yash Banerjee <yashbanerjee82@gmail.com>
2026-06-28 16:39:31 +0530

    Update Google Maps embed URL

--- src/config/contact.js
+++ src/config/contact.js
@@ -13,8 +13,7 @@ export const CONTACT_DETAILS = {
     "+91 77639 95942"
   ],
   email: "info@artoflivingdoranda.org",
-  
-  mapUrl: "<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3663.279250116182!2d85.30784887562022!3d23.341895478951216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f4e04b88a95eab%3A0xf0d15e6916cb2ca0!2sArt%20of%20Living%20Meditation%20Centre!5e0!3m2!1sen!2sin!4v1782643835695!5m2!1sen!2sin"
+  mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3663.279250116182!2d85.30784887562022!3d23.341895478951216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f4e04b88a95eab%3A0xf0d15e6916cb2ca0!2sArt%20of%20Living%20Meditation%20Centre!5e0!3m2!1sen!2sin!4v1782643835695!5m2!1sen!2sin"
   socials: {
     facebook: "https://facebook.com/artofliving",
     instagram: "https://www.instagram.com/artoflivingranchi?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
